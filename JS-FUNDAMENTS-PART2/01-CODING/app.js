dcf7b6b3de9d6080cc547c95585d17f68f0a43fa
martin <gagochorenmartin@gmail.com>
2022-05-06 15:00:09 +0200

    cosas

--- JS-FUNDAMENTS-PART2/01-CODING/app.js
+++ JS-FUNDAMENTS-PART2/01-CODING/app.js
@@ -1,3 +1,36 @@
 "use strict";
 
-console.log("a")
+function mezcladorFrutas(pera,manzana) {
+    var clpera = (pera * 60);
+    var clmanzana = (manzana * 75);
+    var zumo = clpera + clmanzana
+    var percentPera = (( clpera / zumo) * 100).toFixed(2);
+    var percentManzana = ((clmanzana / zumo) * 100).toFixed(2);
+    var datosZumo = `${zumo} cl de zumo creado con ${pera} pera/s (${percentPera}%) y ${manzana} manzana/s (${percentManzana}%)`;
+    return datosZumo;
+
+}
+console.log(mezcladorFrutas(2,5));
+
+// DECLARACION DE FUNCION 
+function AnonimacalcAge(daybirth){
+    let year = new Date().getFullYear();
+    const yearsOld = year - daybirth;
+    return yearsOld;
+}
+
+// FUNCION EXPRESSION 
+ const calcAge = function(daybirth){
+    let year = new Date().getFullYear();
+    const yearsOld = year - daybirth;
+    return yearsOld;
+}
+
+// FUNCION FLECHA
+
+const calcAge2 = (platano,pera) => {
+ let zumo = platano + pera;
+ return zumo;
+}
+
+console.log(AnonimacalcAge(1997), calcAge(1997))
