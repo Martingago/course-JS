87066f0110a653c565b1e96b0a92fad49f97375e
Martin <gagochorenmartin@gmail.com>
2022-05-08 23:40:40 +0200

    arrays

--- JS-FUNDAMENTS-PART2/01-CODING/app.js
+++ JS-FUNDAMENTS-PART2/01-CODING/app.js
@@ -1,26 +1,28 @@
 "use strict";
 
-function mezcladorFrutas(pera,manzana) {
+// PRACTICA
+
+function mezcladorFrutas(pera, manzana) {
     var clpera = (pera * 60);
     var clmanzana = (manzana * 75);
     var zumo = clpera + clmanzana
-    var percentPera = (( clpera / zumo) * 100).toFixed(2);
+    var percentPera = ((clpera / zumo) * 100).toFixed(2);
     var percentManzana = ((clmanzana / zumo) * 100).toFixed(2);
     var datosZumo = `${zumo} cl de zumo creado con ${pera} pera/s (${percentPera}%) y ${manzana} manzana/s (${percentManzana}%)`;
     return datosZumo;
 
 }
-console.log(mezcladorFrutas(2,5));
+console.log(mezcladorFrutas(2, 5));
 
 // DECLARACION DE FUNCION 
-function AnonimacalcAge(daybirth){
+function AnonimacalcAge(daybirth) {
     let year = new Date().getFullYear();
-    const yearsOld = year - daybirth;
+    const yearsOld = `Las personas nacidas en el año ${daybirth}, tienen ahora mismo ${year - daybirth} años`
     return yearsOld;
 }
-
+console.log(AnonimacalcAge(1997))
 // FUNCION EXPRESSION 
- const calcAge = function(daybirth){
+const calcAge = function (daybirth) {
     let year = new Date().getFullYear();
     const yearsOld = year - daybirth;
     return yearsOld;
@@ -28,9 +30,76 @@ function AnonimacalcAge(daybirth){
 
 // FUNCION FLECHA
 
-const calcAge2 = (platano,pera) => {
- let zumo = platano + pera;
- return zumo;
+const calcAge2 = (platano, pera) => {
+    let zumo = platano + pera;
+    return zumo;
 }
 
-console.log(AnonimacalcAge(1997), calcAge(1997))
+
+// FUNCION DENTRO DE FUNCION
+
+const cortadorFrutaSlice = (fruta) => {
+    let slice = fruta * 4;
+    return slice
+}
+
+const fabricarZumo = (manzana, pera) => {
+    let zumoManzana = Number((cortadorFrutaSlice(manzana) * 0.5).toFixed(2));
+    let zumoPera = Number((cortadorFrutaSlice(pera) * 0.4).toFixed(2));
+    let zumoTotal = `${zumoManzana} ml + ${zumoPera}ml, en total ${zumoManzana + zumoPera} ml de zumo `;
+    return zumoTotal;
+}
+
+console.log(fabricarZumo(4, 3));
+
+
+// CODE CHALLENGE 01
+
+// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new 
+// gymnastics discipline, which works differently.
+// Each team competes 3 times, and then the average of the 3 scores is calculated (so 
+// one average score per team).
+// A team only wins if it has at least double the average score of the other team. 
+// Otherwise, no team wins!
+// Your tasks:
+// 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+// 2. Use the function to calculate the average for both teams
+// 3. Create a function 'checkWinner' that takes the average score of each team 
+// as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner 
+// to the console, together with the victory points, according to the rule above. 
+// Example: "Koalas win (30 vs. 13)"
+// 4. Use the 'checkWinner' function to determine the winner for both Data 1 and 
+// Data 2
+// 5. Ignore draws this time
+// Test data:
+// § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+// § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+// Hints:
+// § To calculate average of 3 values, add them all together and divide by 3
+// § To check if number A is at least double number B, check for A >= 2 * B. 
+// Apply this to the team's average scores �
+// GOOD LUCK 
+
+let calcularMedia = (a, b, c) => {
+    let media = (a + b + c) / 3;
+    return media;
+}
+
+let puntuacionDolphins = calcularMedia(85, 54, 41);
+let puntuacionKoalas = calcularMedia(23, 34, 27);
+
+let checkWinner = (puntosDelfines, puntosKoalas) => {
+    puntosDelfines > puntosKoalas ? console.log(`Delfines ganan el partido: ${puntosDelfines} pts a ${puntosKoalas} pts`)
+        : console.log(`Koalas ganan el partido ${puntosKoalas}pts  a ${puntosDelfines} pts`);
+    if (puntosDelfines >= puntosKoalas * 2) {
+        console.log("la puntuacion de los delfines es el doble que de los koalas")
+    } else if (puntosKoalas >= puntosDelfines * 2) {
+        console.log("la puntuacion de los Koalas es el doble que de los Delfines")
+    } else if (puntosKoalas == puntosDelfines) {
+        console.log("ha habido un empate")
+    }
+    else {
+        console.log("nadie ha puntuado el doble")
+    }
+}
+checkWinner(puntuacionDolphins, puntuacionKoalas);
---
+++ JS-FUNDAMENTS-PART2/02-CODING/app.js
@@ -0,0 +1,68 @@
+"use strict";
+
+const nombres = ["Martin", "Pepe", "Juan"];
+const edad = new Array("29", "18", "23")
+
+console.log(nombres, edad);
+
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of 
+// the bill if the bill value is between 50 and 300, and if the value is different, the tip is 
+// 20%.
+// Your tasks:
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns 
+// the corresponding tip, calculated based on the rules above (you can check out 
+// the code from first tip calculator challenge if you need to). Use the function 
+// type you like the most. Test the function using a bill value of 100
+// 2. And now let's use arrays! So create an array 'bills' containing the test data 
+// below
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from 
+// the function you created before
+// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+// Test data: 125, 555 and 44
+// Hint: Remember that an array needs a value in each position, and that value can 
+// actually be the returned value of a function! So you can just call a function as array 
+// values (so don't store the tip values in separate variables first, but right in the new 
+// array) �
+// GOOD LUCK �
+
+const calcTip = (value) => {
+    let tip = 0;
+    if (value >= 50 && value <= 300) {
+        tip = value * 0.15;
+    } else {
+        tip = value * 0.20
+    }
+    let totalAmount = value + tip
+    return tip;
+}
+//  FORMA SIMPLIFICADA DE LA FUNCION EMPLEANDO ? Y : 
+ 
+const calcTip2 = (bill) => {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+
+}
+
+console.log(calcTip(80), calcTip2(80))
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+console.log(total)
+
+// OBJETOS  
+
+const martin = {
+    firstName: 'Martin',
+    lastName: 'Gago',
+    age: 24,
+    job: 'programador',
+    friends: ['maria', 'marcos', 'xiana']
+};
+
+const ageKey = 'age';
+console.log(martin.firstName);
+console.log(martin['lastName']);
+console.log(martin[ageKey]);
+
+console.log(`${martin.firstName} tiene ${martin.friends.length} amigos, 
+y su mejor amigo se llama: ${martin.friends[1]}`)
